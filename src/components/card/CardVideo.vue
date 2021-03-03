5613361daaa1dfc04cedafc23120fc4b4ccf79eb
celebitolga <tolgacelebi78@gmail.com>
2021-03-03 20:41:12 +0300

    Svg icons added to video

--- src/components/card/CardVideo.vue
+++ src/components/card/CardVideo.vue
@@ -10,7 +10,30 @@
     <!-- Video controllers -->
     <div class="playerControls">
       <!-- Play - Pause button -->
-      <button @click="toggleVideo()" class="playerControls-button">{{playing ? '►' : '❚❚'}}</button>
+      <div @click="toggleVideo()" class="playerControls-button" v-if="playing">
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          viewBox="0 0 298.667 298.667" style="enable-background:new 0 0 298.667 298.667;" xml:space="preserve">
+          <g>
+            <g>
+              <polygon points="32,0 32,298.667 266.667,149.333 		"/>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div @click="toggleVideo()" class="playerControls-button" v-else>
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 298.667 298.667" style="enable-background:new 0 0 298.667 298.667;" xml:space="preserve">
+          <g>
+            <g>
+              <g>
+                <rect x="192" y="0" width="85.333" height="298.667"/>
+                <rect x="21.333" y="0" width="85.333" height="298.667"/>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <!-- <button @click="toggleVideo()" class="playerControls-button">{{playing ? '►' : '❚❚'}}</button> -->
       
       <!-- Change video/audio time and progress bar -->
       <div @click="changeVideoTime($event)" ref="playerControlsProgress" class="playerControls-progress">
@@ -19,12 +42,59 @@
 
       <!-- Change video/audio volume -->
       <div v-if="hasSound" class="playerControls-volume">
-        <button @click="toggleAudio()" class="playerControls-volume-volumeButton"> {{sound ? '+' : '-'}} </button>
+        <div @click="toggleAudio()" class="playerControls-volume-volumeButton" v-if="sound">
+          <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+            <g>
+              <g>
+                <g>
+                  <path d="M288,192c0-37.653-21.76-70.187-53.333-85.867v171.84C266.24,262.187,288,229.653,288,192z"/>
+                  <polygon points="0,128 0,256 85.333,256 192,362.667 192,21.333 85.333,128 			"/>
+                  <path d="M234.667,4.907V48.96C296.32,67.307,341.333,124.373,341.333,192S296.32,316.693,234.667,335.04v44.053
+                    C320.107,359.68,384,283.413,384,192S320.107,24.32,234.667,4.907z"/>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div @click="toggleAudio()" class="playerControls-volume-volumeButton" v-else>
+          <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              viewBox="0 0 384 384" style="enable-background:new 0 0 384 384;" xml:space="preserve">
+            <g>
+              <g>
+                <g>
+                  <path d="M288,192c0-37.653-21.76-70.187-53.333-85.867v47.147l52.373,52.373C287.68,201.173,288,196.587,288,192z"/>
+                  <path d="M341.333,192c0,20.053-4.373,38.933-11.52,56.32l32.32,32.32C376,254.08,384,224,384,192
+                    c0-91.307-63.893-167.68-149.333-187.093V48.96C296.32,67.307,341.333,124.373,341.333,192z"/>
+                  <polygon points="192,21.333 147.413,65.92 192,110.507 			"/>
+                  <path d="M27.2,0L0,27.2L100.8,128H0v128h85.333L192,362.667V219.2l90.773,90.773c-14.293,10.987-30.4,19.84-48.107,25.173V379.2
+                    c29.333-6.72,56.107-20.16,78.613-38.613L356.8,384l27.2-27.2l-192-192L27.2,0z"/>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </div>
+        <!-- <button @click="toggleAudio()" class="playerControls-volume-volumeButton"> {{sound ? '+' : '-'}} </button> -->
         <input @change="volumeChange($event)" type="range" class="playerControls-volume-volumeRange" min="0" max="1" step="0.05" :value="volume">
       </div>
 
       <!-- Fullscreen button -->
-      <button @click="openFullscreen()" class="playerControls-fullScreen">FullScreen</button>
+      <!-- <button @click="openFullscreen()" class="playerControls-fullScreen">FullScreen</button> -->
+      <div @click="openFullscreen()" class="playerControls-fullScreen">
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          viewBox="0 0 298.667 298.667" style="enable-background:new 0 0 298.667 298.667;" xml:space="preserve">
+          <g>
+            <g>
+              <g>
+                <polygon points="42.667,192 0,192 0,298.667 106.667,298.667 106.667,256 42.667,256 			"/>
+                <polygon points="0,106.667 42.667,106.667 42.667,42.667 106.667,42.667 106.667,0 0,0 			"/>
+                <polygon points="192,0 192,42.667 256,42.667 256,106.667 298.667,106.667 298.667,0 			"/>
+                <polygon points="256,256 192,256 192,298.667 298.667,298.667 298.667,192 256,192 			"/>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
 
     </div>
     <!-- /// Video controllers -->
@@ -88,7 +158,7 @@ export default {
       this.volume = $event.target.value;
       this.$store.commit('setOldVolume', this.volume);
       this.$refs.audioAutoPlay.volume = this.volume;
-      if(!this.sound && this.volume > 0) {
+      if(!this.sound && this.playing && this.volume > 0) {
         this.$refs.audioAutoPlay.play();
         this.$refs.audioAutoPlay.muted = false;
         this.sound = true;
@@ -200,6 +270,7 @@ export default {
 .playerControls {
   position: absolute;
   bottom: 0px;
+  background: rgba(0, 0, 0, 0.2);
   display: flex;
   padding: 10px;
   opacity: 0;
@@ -209,6 +280,14 @@ export default {
   &-button {
     outline: none;
     border: none;
+    width: 32.5px;
+    height: 24px;
+    
+    &:hover {
+      svg {
+        fill: rgba(255, 255, 255, 0.5);
+      }
+    }
   }
 
   &-progress {
@@ -221,7 +300,7 @@ export default {
     background: rgba(7, 104, 214, 0.5);
     align-self: center;
     margin: 0 10px;
-    cursor: ew-resize;
+    cursor: cursor;
 
     &-progressFilled {
       background: #ffc600;
@@ -238,10 +317,10 @@ export default {
       opacity: 0;
       padding: 10px 0;
       position: absolute;
-      right: 0;
+      right: 50%;
       width: 50px;
       height: 30px;
-      transform: rotateZ(-90deg) translateX(120%) ;
+      transform: rotateZ(-90deg) translateX(170%) translateY(50%);
 
       &:hover {
         opacity: 1;
@@ -249,9 +328,27 @@ export default {
     }
 
     &-volumeButton {
+      width: 24px;
+      height: 24px;
 
       &:hover + .playerControls-volume-volumeRange {
         opacity: 1;
+      }
+      &:hover {
+        svg {
+          fill: rgba(255, 255, 255, 0.5);
+        }
+      }
+    }
+  }
+
+  &-fullScreen {
+    width: 29.3px;
+    height:22px;
+
+    &:hover {
+      svg {
+        fill: rgba(255, 255, 255, 0.5);
       }
     }
   }
