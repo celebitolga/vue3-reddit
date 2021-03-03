93903183f71675b48d8946205ea3224b54eeaa96
celebitolga <tolgacelebi78@gmail.com>
2021-03-03 03:28:15 +0300

    Some audio update and controllers ui

--- src/components/card/CardVideo.vue
+++ src/components/card/CardVideo.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="card-image waves-effect waves-block waves-light videoControl">
-    <video ref="videoRef" class="center videoAutoPlay" loop="loop" muted="muted" webkit-playsinline="true" playsinline="true" :mediagroup="getVideoUrl()">
+    <video @click="toggleVideo()" ref="videoRef" class="center videoAutoPlay" loop="loop" muted="muted" webkit-playsinline="true" playsinline="true" :mediagroup="getVideoUrl()">
       <source :src="getVideoUrl()" type="video/mp4">
     </video>
     <audio ref="audioAutoPlay" loop="loop" muted="muted" :mediagroup="getVideoUrl()" type="audio/mp4">
         <source :src="getVideoAudio()" playsinline="true">
     </audio>
     <div class="playerControls">
-      <button @click="playAudio()" class="playerControls-button">{{playing ? '►' : '❚❚'}}</button>
+      <button @click="toggleVideo()" class="playerControls-button">{{playing ? '►' : '❚❚'}}</button>
       <div class="playerControls-progress">
         <div ref="progressFilled" class="playerControls-progress-progressFilled"></div>
+      </div>
+      <div v-if="hasSound" class="playerControls-volume">
+        <button @click="toggleAudio()" class="playerControls-volume-volumeButton"> {{sound ? '+' : '-'}} </button>
+        <input @change="volumeChange($event)" type="range" name="volume" class="playerControls-volume-volumeRange" min="0" max="1" step="0.05" :value="volume">
       </div>
       <button class="playerControls-fullScreen">FullScreen</button>
     </div>
@@ -21,6 +25,9 @@ export default {
   data() {
     return {
       volume: 0.0,
+      oldVolume: 0.0,
+      hasSound: false,
+      sound: false,
       playing: false,
       playButton: true,
     }
@@ -32,29 +39,41 @@ export default {
     getVideoAudio() {
       return this.videoUrl.substring(0,32) + 'DASH_audio.mp4';
     },
-    playAudio() {
+    toggleVideo() {
       if(this.playButton) {
-        this.$refs.audioAutoPlay.pause();
         this.$refs.videoRef.pause();
+        this.$refs.audioAutoPlay.pause();
         this.playButton = false;
         this.playing = false;
       } else {
-        this.$refs.audioAutoPlay.currentTime = this.$refs.videoRef.currentTime;
-        this.$refs.audioAutoPlay.play();
         this.$refs.videoRef.play();
+        this.$refs.audioAutoPlay.play();
         this.playButton = true;
         this.playing = true;
+      }
+    },
+    volumeChange($event) {
+      this.volume = $event.target.value;
+      this.$refs.audioAutoPlay.volume = this.volume;
+      if(!this.sound && this.volume > 0) {
+        this.$refs.audioAutoPlay.play();
+        this.$refs.audioAutoPlay.muted = false;
+        this.sound = true;
+      } else if(this.volume == 0) {
+        this.$refs.audioAutoPlay.pause();
+        this.$refs.audioAutoPlay.muted = true;
+        this.sound = false;
       }
     },
     playVideo() {
       const playVideo = this.$refs.videoRef;
       const audio = this.$refs.audioAutoPlay;
-
+        
       if(playVideo != null) {
-          const slideInAt = (window.scrollY + window.innerHeight) - playVideo.offsetHeight / 2;
-          const isHalfShown = slideInAt > playVideo.parentNode.parentElement.offsetTop;
-          const videoBottom = playVideo.parentNode.parentElement.offsetTop + playVideo.offsetHeight;
-          const isNotScrolledPast = window.scrollY < videoBottom;
+        const slideInAt = (window.scrollY + window.innerHeight) - playVideo.offsetHeight / 2;
+        const isHalfShown = slideInAt > playVideo.parentNode.parentElement.offsetTop;
+        const videoBottom = playVideo.parentNode.parentElement.offsetTop + playVideo.offsetHeight;
+        const isNotScrolledPast = window.scrollY < videoBottom;
 
         if (isHalfShown && isNotScrolledPast && playVideo.readyState === 4) {
           if(playVideo.paused && !this.playing && this.playButton) {
@@ -73,7 +92,35 @@ export default {
         }
       }
     },
+    toggleAudio() {
+      if(!this.sound && this.volume == 0 && this.oldVolume == 0) {
+        this.volume = 0.5;
+        this.$refs.audioAutoPlay.volume = this.volume;
+      } else if(!this.sound && this.volume == 0 && this.oldVolume != 0) {
+        this.volume = this.oldVolume;
+        this.$refs.audioAutoPlay.volume = this.volume;
+      } else if(!this.sound && this.volume != 0) {
+        this.$refs.audioAutoPlay.volume = this.volume;
+      } else {
+        this.oldVolume = this.volume;
+        this.volume = 0.0;
+      }
+
+      if(!this.$refs.videoRef.paused && this.volume != 0) {
+        this.$refs.audioAutoPlay.play();
+        this.$refs.audioAutoPlay.muted = false;
+        this.sound = true;
+      } else {
+        this.$refs.audioAutoPlay.pause();
+        this.$refs.audioAutoPlay.muted = true;
+        this.sound = false;
+      }
+    },
     handleProgress() {
+      if(this.$refs.audioAutoPlay.duration > 0) {
+        this.hasSound = true;
+        this.$refs.audioAutoPlay.currentTime = this.$refs.videoRef.currentTime;
+      }
       const percent = (this.$refs.videoRef.currentTime / this.$refs.videoRef.duration) * 100;
       this.$refs.progressFilled.style.flexBasis = `${ percent }%`;
     },
@@ -108,13 +155,21 @@ export default {
     max-width: 640px;
     max-height: 650px;
   }
+
+  &:hover .playerControls{
+    opacity: 1;
+    transition: all .3s;
+  }
 }
 
 .playerControls {
   position: absolute;
-  bottom: 0;
+  bottom: 0px;
   display: flex;
+  padding: 10px;
+  opacity: 0;
   width: 100%;
+  transition: all .3s;
 
   &-button {
     // background-color: rgba(0,0,0,0.5);
@@ -136,6 +191,21 @@ export default {
 
     &-progressFilled {
       background: #ffc600;
+    }
+  }
+
+  &-volume {
+    position: relative;
+    width: 50px;
+
+    &-volumeButton {
+
+    }
+    &-volumeRange {
+      position: absolute;
+      right: 0;
+      width: 50px;
+      transform: rotateZ(-90deg) translate(90%, -100%) ;
     }
   }
 }
