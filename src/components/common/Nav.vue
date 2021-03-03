bd50a63e5a01bebc70a74522162d31ea037c3176
celebitolga <tolgacelebi78@gmail.com>
2021-03-03 20:53:05 +0300

    nothing

--- src/components/common/Nav.vue
+++ src/components/common/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <div class="nav-wrapper light-blue darken-4">
+    <div class="nav-wrapper light-blue">
       <div class="container">
         <router-link tag="a" to="/" class="brand-logo">Reddit Client</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
